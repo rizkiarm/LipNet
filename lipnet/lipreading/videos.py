a5a1baed32d9e38f63ec9b89e2445a7d234ef29e
Muhammad Rizki A.R.M <rizki.idea@gmail.com>
2017-06-20 22:24:14 +0700

    Better approach to get consistent mouth crop

--- lipnet/lipreading/videos.py
+++ lipnet/lipreading/videos.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 from keras import backend as K
 from scipy import ndimage
+from scipy.misc import imresize
 import skvideo.io
 import dlib
 from lipnet.lipreading.aligns import Align
@@ -146,6 +147,10 @@ class Video(object):
         self.set_data(frames)
 
     def get_frames_mouth(self, detector, predictor, frames):
+        MOUTH_WIDTH = 100
+        MOUTH_HEIGHT = 50
+        HORIZONTAL_PAD = 0.19
+        normalize_ratio = None
         mouth_frames = []
         for frame in frames:
             dets = detector(frame, 1)
@@ -158,12 +163,31 @@ class Video(object):
             mouth_points = []
             for part in shape.parts():
                 i += 1
-                if i < 48:
+                if i < 48: # Only take mouth region
                     continue
                 mouth_points.append((part.x,part.y))
             np_mouth_points = np.array(mouth_points)
-            mouth_centroid = np.mean(np_mouth_points[:,-2:], axis=0).astype(int)
-            mouth_crop_image = frame[mouth_centroid[1]-25:mouth_centroid[1]+25,mouth_centroid[0]-50:mouth_centroid[0]+50]
+
+            mouth_centroid = np.mean(np_mouth_points[:, -2:], axis=0)
+
+            if normalize_ratio is None:
+                mouth_left = np.min(np_mouth_points[:, :-1]) * (1.0 - HORIZONTAL_PAD)
+                mouth_right = np.max(np_mouth_points[:, :-1]) * (1.0 + HORIZONTAL_PAD)
+
+                normalize_ratio = MOUTH_WIDTH / float(mouth_right - mouth_left)
+
+            new_img_shape = (int(frame.shape[0] * normalize_ratio), int(frame.shape[1] * normalize_ratio))
+            resized_img = imresize(frame, new_img_shape)
+
+            mouth_centroid_norm = mouth_centroid * normalize_ratio
+
+            mouth_l = int(mouth_centroid_norm[0] - MOUTH_WIDTH / 2)
+            mouth_r = int(mouth_centroid_norm[0] + MOUTH_WIDTH / 2)
+            mouth_t = int(mouth_centroid_norm[1] - MOUTH_HEIGHT / 2)
+            mouth_b = int(mouth_centroid_norm[1] + MOUTH_HEIGHT / 2)
+
+            mouth_crop_image = resized_img[mouth_t:mouth_b, mouth_l:mouth_r]
+
             mouth_frames.append(mouth_crop_image)
         return mouth_frames
 
