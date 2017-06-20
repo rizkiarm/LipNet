b6a0f3be8eb8f4d908268901444dcd1a7ee02056
Muhammad Rizki A.R.M <rizki.idea@gmail.com>
2017-06-20 23:19:01 +0700

    Remove old unused scripts

--- scripts/convert_to_frames.sh
+++
@@ -1,9 +0,0 @@
-LIST=$(ls 0_Data/$1/*)
-
-for VID in $LIST
-do
-    NEW="${VID/0_Data/1_DataFrames}" # Target
-    NEW="${NEW/.mpg/}"
-    mkdir -p $NEW
-    ffmpeg -i $VID -f image2 $NEW/image%03d.png
-done
--- scripts/create_mouthcropsmall_data.sh
+++
@@ -1,8 +0,0 @@
-LIST=$(ls -d 1_DataFrames/$1/*/)
-
-for VID in $LIST
-do
-    NEW="${VID/1_DataFrames/2_DataMouthCropSmall}" # Target
-    mkdir -p $NEW
-    python face_landmark_detection.py shape_predictor_68_face_landmarks.dat $VID $NEW
-done
