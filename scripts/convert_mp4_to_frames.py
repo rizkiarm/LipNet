ae92ad79dcad7c4800c94a6756a520ed9f8e918b
Alexei Kouminov <akoumino@cisco.com>
2020-04-29 03:36:40 -0400

    adjustements for extract mouth

---
+++ scripts/convert_mp4_to_frames.py
@@ -0,0 +1,16 @@
+# create a folder to store extracted images
+import os
+folder = 'test'
+os.mkdir(folder)
+# use opencv to do the job
+import cv2
+print(cv2.__version__)  # my version is 3.1.0
+vidcap = cv2.VideoCapture('swwv9a_mouth.mp4')
+count = 0
+while True:
+    success,image = vidcap.read()
+    if not success:
+        break
+    cv2.imwrite(os.path.join(folder,"frame{:d}.jpg".format(count)), image)     # save frame as JPEG file
+    count += 1
+print("{} images are extacted in {}.".format(count,folder))
