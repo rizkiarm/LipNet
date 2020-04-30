b86856f7a373a0f1bde4df205d1d34939b6445c1
Alexei Kouminov <akoumino@cisco.com>
2020-04-29 23:24:07 -0400

    get rid of model4 wrong model

--- training/overlapped_speakers/train.py
+++ training/overlapped_speakers/train.py
@@ -61,7 +61,7 @@ def train(run_name, speaker, start_epoch, stop_epoch, img_c, img_w, img_h, frame
     csv_logger  = CSVLogger(os.path.join(LOG_DIR, "{}-{}.csv".format('training',run_name)), separator=',', append=True)
     checkpoint  = ModelCheckpoint(os.path.join(OUTPUT_DIR, run_name, "weights{epoch:02d}.h5"), monitor='val_loss', save_weights_only=True, mode='auto', period=1)
 
-    lipnet.model4.fit_generator(generator=lip_gen.next_train(),
+    lipnet.model.fit_generator(generator=lip_gen.next_train(),
                         steps_per_epoch=lip_gen.default_training_steps, epochs=stop_epoch,
                         validation_data=lip_gen.next_val(), validation_steps=lip_gen.default_validation_steps,
                         callbacks=[checkpoint, statistics, visualize, lip_gen, tensorboard, csv_logger],
